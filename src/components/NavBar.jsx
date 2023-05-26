6de3cf8032bd890c6e14ee66548e405936379d6c
Derek Wan <d.wan@icloud.com>
2023-05-26 08:56:43 +0800

    Initial layout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+export default function NavBar(props) {
+  const { titleText, buttonText, setPage, targetPage } = props;
+  return (
+    <nav>
+      <h1>{titleText}</h1>
+      <button onClick={() => setPage(targetPage)}>{buttonText}</button>
+    </nav>
+  );
+}
